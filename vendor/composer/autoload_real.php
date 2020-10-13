2bd8065b555b6bf23e603b38bf5d0ec9593c2f5a
daniel <daniellim4400@gmail.com>
2020-10-13 10:31:35 +0700

    vendor commit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInit632f8a3ee85b0871807bad076a94950f
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
