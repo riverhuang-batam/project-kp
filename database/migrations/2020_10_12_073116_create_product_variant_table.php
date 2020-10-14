ded6c7acc4dbd6f36c8067c5e08f6d0cc132243f
Kade <sugiantara.igk@gmail.com>
2020-10-14 16:01:14 +0700

    update migration

--- database/migrations/2020_10_12_073116_create_product_variant_table.php
+++ database/migrations/2020_10_12_073116_create_product_variant_table.php
@@ -18,8 +18,6 @@ class CreateProductVariantTable extends Migration
             $table->foreignId('product_id')->references('id')->on('products');
             $table->string('name', 100);
             $table->double('unit_price', 8, 2)->nullable();
-            $table->integer('quantity')->nullable();
-            $table->double('sub_total', 8, 2)->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
