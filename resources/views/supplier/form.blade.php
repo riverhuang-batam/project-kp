84867e0e09939b1a626759d692af5ddfb40921bc
Kade <sugiantara.igk@gmail.com>
2020-10-23 09:16:26 +0700

    edit supplier title

--- resources/views/supplier/form.blade.php
+++ resources/views/supplier/form.blade.php
@@ -4,15 +4,24 @@
 @section('contents')
 <div class="page-wrapper">
   <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">
-          <div class="card-title m-0">
-            <h5>
-            {{isset($supplier) ? 'Edit Existing' : 'Add New'}} Supplier
-            </h5>
-          </div>
+    <div class="col-md-12">
+      <div class="page-breadcrumb">
+        <div class="row">
+            <div class="col-7 align-self-center">
+              <h4 class="page-title text-truncate text-dark font-weight-medium mb-1">{{isset($supplier) ? 'Edit Existing' : 'Add New'}} Supplier</h4>
+                <div class="d-flex align-items-center">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb m-0 p-0">
+                            <li class="breadcrumb-item"><a href="{{route('home')}}" class="text-muted">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{route('suppliers.index')}}" class="text-muted">Supplier</a></li>
+                            <li class="breadcrumb-item text-muted active" aria-current="page">{{isset($supplier) ? 'Edit' : 'Add'}} Supplier</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
         </div>
+      </div>
+      <div class="card">
         <div class="card-body">
           <form method="POST" enctype="multipart/form-data"
             action="{{ isset($supplier) ? route('suppliers.update', $supplier['id']) : route('suppliers.store') }}">
