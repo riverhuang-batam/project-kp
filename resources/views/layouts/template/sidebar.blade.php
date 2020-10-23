9bc7148a022c781d6d5e8f36276619de487a6fa6
Kade <sugiantara.igk@gmail.com>
2020-10-23 10:02:51 +0700

    edit header and side bar

--- resources/views/layouts/template/sidebar.blade.php
+++ resources/views/layouts/template/sidebar.blade.php
@@ -33,18 +33,6 @@
                             class="hide-menu">Payment
                               </span></a>
                       </li>
-                      <li class="list-divider"></li>
-                      <li class="sidebar-item">
-                      <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                                <i data-feather="power"
-                                class="svg-icon mr-2 ml-1"></i>
-                                    Logout
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                                </form>
-                      </li>
                   </ul>
               </nav>
               <!-- End Sidebar navigation -->
